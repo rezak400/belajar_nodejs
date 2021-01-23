615e55f0ed349bc36b811a406d55b93067dacf63
rezak400 <k400ezza@gmail.com>
2021-01-23 11:56:47 +0700

    add new

---
+++ app.mjs
@@ -0,0 +1,5 @@
+import User from './modul1.mjs'
+console.log(`Hello from index node`)
+
+let user = new User('reza')
+console.log(user)
---
+++ modul1.mjs
@@ -0,0 +1,9 @@
+export default class Human{
+    constructor(name){
+        this.name = name
+    }
+}
+
+let printName = (name) => {
+    console.log(`Hello ${nama}`)
+}
